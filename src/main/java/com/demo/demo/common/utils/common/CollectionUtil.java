3ef00945bf1cb2e75b5319cfe0740740729b764a
liuyang <yang.liu@mail.dealmoon.com>
2018-12-25 15:07:42 +0800

    1

--- src/main/java/com/demo/demo/common/utils/common/CollectionUtil.java
+++ src/main/java/com/demo/demo/common/utils/common/CollectionUtil.java
@@ -2,7 +2,6 @@ package com.demo.demo.common.utils.common;
 
 import org.springframework.util.CollectionUtils;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
